021a690f033fe5af9bb292ecfef2a2d57e6252ab
Taimoor khan <khant8k83@gmail.com>
2021-08-16 15:04:57 +0500

    handling error and clear exit message

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     // env::args() returns an iterator of command line arguments
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
-    println!("Searching for: {:?} \nin: {:?}", config.query, config.filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error parsing the arguments: {}", err);        
+        process::exit(1);
+    });
+    println!(
+        "Searching for: {:?} \nin: {:?}",
+        config.query, config.filename
+    );
 
     let contents = fs::read_to_string("./poem.txt").expect("something went wrong!");
     println!("with contents: \n{}", contents);
@@ -17,9 +23,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        };
+
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+
+        Ok(Config { query, filename })
     }
 }
