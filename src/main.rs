4b7bb5be1dc5e0449ddfb626f38cf08124aebad7
Taimoor khan <khant8k83@gmail.com>
2021-08-16 14:24:15 +0500

    refactor env config fn

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use std::{env, fs}; 
+use std::{env, fs};
 
 fn main() {
     // env::args() returns an iterator of command line arguments
-    let args : Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for: {:?} \nin: {:?}", query, filename);
+    let args: Vec<String> = env::args().collect();
 
-    let contents = fs::read_to_string("./poem.txt")
-        .expect("something went wrong!");
-    println!("with contents: \n{}", contents)
+    let config = Config::new(&args);
+    println!("Searching for: {:?} \nin: {:?}", config.query, config.filename);
+
+    let contents = fs::read_to_string("./poem.txt").expect("something went wrong!");
+    println!("with contents: \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
