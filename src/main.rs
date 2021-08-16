71cdfab31929bb696358c07639715e7e0d37444b
Taimoor khan <khant8k83@gmail.com>
2021-08-16 13:09:44 +0500

    access cli args and store in variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env; 
+
 fn main() {
-    println!("Hello, world!");
+    // env::args() returns an iterator of command line arguments
+    let args : Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for: {:?} \n in: {:?}", query, filename);
 }
