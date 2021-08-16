60decd03827715cc0060c1ca36744de0444c0096
Taimoor khan <khant8k83@gmail.com>
2021-08-16 13:17:33 +0500

    reading file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::env; 
+use std::{env, fs}; 
 
 fn main() {
     // env::args() returns an iterator of command line arguments
     let args : Vec<String> = env::args().collect();
     let query = &args[1];
     let filename = &args[2];
-    println!("Searching for: {:?} \n in: {:?}", query, filename);
+    println!("Searching for: {:?} \nin: {:?}", query, filename);
+
+    let contents = fs::read_to_string("./poem.txt")
+        .expect("something went wrong!");
+    println!("with contents: \n{}", contents)
 }
