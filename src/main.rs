2a1aafdf662d0f8a8b381176cb70101d6b4c69a9
Taimoor khan <khant8k83@gmail.com>
2021-09-11 15:24:16 +0500

    improved using iterators

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use minigrep::Config;
 
 fn main() {
     // env::args() returns an iterator of command line arguments
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
     // unwrap_or_else extracts OK value  and helps to define non panic errors
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         // this will send error to standard error stream
         eprintln!("Error parsing the arguments: {}", err);
         process::exit(1);
